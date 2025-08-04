5a42fab3afbf0431019970344aecec074cc8dca8
me-Sushil <girbahadurbk123@gmail.com>
2025-08-04 23:59:24 +0545

    seperate the handler

--- part1/bJavaScript/usestate/src/App.jsx
+++ part1/bJavaScript/usestate/src/App.jsx
@@ -3,14 +3,13 @@ import { useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
   console.log(counter);
-  // const handleClick=()=>{
-  //   console.log("clicked")
-  // }
+  const handlePlus=()=>setCounter(counter+1);
+  const handleReset=()=>setCounter(0);
   return (
     <>
     <p>Count is: {counter}</p>
-      <button onClick={()=>{setCounter(counter+1)}}>Plus</button>
-      <button onClick={()=>{setCounter(0)}}>Reset</button>
+      <button onClick={handlePlus}>Plus</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 };
