846d708466ec177107e1ad2dc160c242c062f4a2
me-Sushil <girbahadurbk123@gmail.com>
2025-08-04 17:25:48 +0545

    practice same problem by myself

--- part1/bJavaScript/array-using/src/App.jsx
+++ part1/bJavaScript/array-using/src/App.jsx
@@ -1,26 +1,47 @@
+// import SayHello from "./SayHello";
+
+// const App = () => {
+//   let persons = [
+//     { fName: "Sushil", lName: "Bishowkarma", id: 202 },
+//     { fName: "Sudesh", lName: "Mateh", id: 101 },
+//     { fName: "Sugam", lName: "Mateh", id: 303 },
+//   ];
+
+//   return (
+//     <div>
+//       <h1 className="hello">Hello World</h1>
+//       {/*conditions ? true : false*/}
+//       {persons.length > 0 ? (
+//         persons.filter(person => person.id > 200).map((person) => <SayHello person={person} key={person.id} />)
+//       ) : (
+//         <p>There is no item</p>
+//       )}
+
+//       {/* <h1 className="hello">Hello World</h1>
+//       <SayHello name="Sushil"/>
+//       <SayHello name="Sudesh"/>
+//       <SayHello name="Sugam"/> */}
+//     </div>
+//   );
+// };
+// export default App;
+
 import SayHello from "./SayHello";
-
 const App = () => {
-  let persons = [
-    { fName: "Sushil", lName: "Bishowkarma", id: 202 },
-    { fName: "Sudesh", lName: "Mateh", id: 101 },
-    { fName: "Sugam", lName: "Mateh", id: 303 },
+  const persons = [
+    { fname: "Sushil", lname: "Bishowkarma", id: 202 },
+    { fname: "Sudesh", lname: "Mateh", id: 303 },
+    { fname: "Sugam", lname: "Mateh", id: 200 },
   ];
-
   return (
     <div>
-      <h1 className="hello">Hello World</h1>
-      {/*conditions ? true : false*/}
       {persons.length > 0 ? (
-        persons.filter(person => person.id > 200).map((person) => <SayHello person={person} key={person.id} />)
+        persons
+          .filter((filter) => filter.id > 200)
+          .map((person) => <SayHello person={person} key={person.id} />)
       ) : (
-        <p>There is no item</p>
+        <p>There is no text</p>
       )}
-
-      {/* <h1 className="hello">Hello World</h1>
-      <SayHello name="Sushil"/>
-      <SayHello name="Sudesh"/>
-      <SayHello name="Sugam"/> */}
     </div>
   );
 };
--- part1/bJavaScript/array-using/src/SayHello.jsx
+++ part1/bJavaScript/array-using/src/SayHello.jsx
@@ -1,13 +1,27 @@
-const SayHello = ({person}) => {
-  //we dont need this in jsx
-  //React.createElement("p", {class:"myP"}, `Hello ${props.name}`);
+// const SayHello = ({person}) => {
+//   //we dont need this in jsx
+//   //React.createElement("p", {class:"myP"}, `Hello ${props.name}`);
 
-  function getFullName(){
-    return `${person.fName} ${person.lName}`;
+//   function getFullName(){
+//     return `${person.fName} ${person.lName}`;
+//   }
+//   return(
+//   <p>Hello {getFullName()}</p>
+//   )
+// };
+
+// export default SayHello;
+
+
+
+
+const SayHello=({person})=>{
+
+  function getName() {
+   return `${person.fname} ${person.lname}`;
   }
-  return(
-  <p>Hello {getFullName()}</p>
-  )
-};
-
+ return(
+   <p>Hello {getName()}</p>
+ ) 
+}
 export default SayHello;
