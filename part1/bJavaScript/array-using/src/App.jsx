bb767084bb616ca04db1106a98f06954816d54b1
me-Sushil <girbahadurbk123@gmail.com>
2025-08-04 13:42:26 +0545

    make unique id for each person

--- part1/bJavaScript/array-using/src/App.jsx
+++ part1/bJavaScript/array-using/src/App.jsx
@@ -2,16 +2,16 @@ import SayHello from "./SayHello";
 
 const App = () => {
   let persons = [
-    { fName: "Sushil", lName: "Bishowkarma" },
-    { fName: "Sudesh", lName: "Mateh" },
-    { fName: "Sugam", lName: "Mateh" },
+    { fName: "Sushil", lName: "Bishowkarma", id:202 },
+    { fName: "Sudesh", lName: "Mateh", id:101 },
+    { fName: "Sugam", lName: "Mateh", id:303 },
   ];
 
   return (
     <div>
       <h1 className="hello">Hello World</h1>
       {persons.map((person) => (
-        <SayHello fName={person.fName} lName={person.lName} />
+        <SayHello fName={person.fName} lName={person.lName} key={person.id} />
       ))}
       {/* <h1 className="hello">Hello World</h1>
       <SayHello name="Sushil"/>
