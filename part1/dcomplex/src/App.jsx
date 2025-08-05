4f6cf8c5a3faeccd830606742a4ebea39160c30d
me-Sushil <girbahadurbk123@gmail.com>
2025-08-05 19:17:22 +0545

    array update in state

--- part1/dcomplex/src/App.jsx
+++ part1/dcomplex/src/App.jsx
@@ -4,18 +4,22 @@ function App() {
   // const [left, setLeft] = useState(0);
   // const [right, setRight] = useState(0);
   const [click, handleClick] = useState({left:0, right:0})
+  const [allClicks, setAll] = useState([]);
 
   const handleLeftClick=()=>{
+    setAll(allClicks.concat("L"));
     handleClick({...click, left: click.left+1});
   }
 
   const handleRightClick=()=>{
+    setAll(allClicks.concat("U"));
     handleClick({...click, right:click.right+1});
   }
 
   return (
     <>
       <div>
+        <p>update arr {allClicks.join(" ")}</p>
         <p>The left btn is clicked {click.left} times</p>
         <button onClick={handleLeftClick}>Left BTN</button>
       </div>
