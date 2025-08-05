ce72c57bb89f9797398207e0098a2b2e4512d28a
me-Sushil <girbahadurbk123@gmail.com>
2025-08-05 22:04:08 +0545

    did some experiment to find out react async behavior

--- part1/dcomplex/src/App.jsx
+++ part1/dcomplex/src/App.jsx
@@ -3,30 +3,52 @@ import { useState } from "react";
 function App() {
   // const [left, setLeft] = useState(0);
   // const [right, setRight] = useState(0);
-  const [click, handleClick] = useState({left:0, right:0})
-  const [allClicks, setAll] = useState([]);
+  // const [click, handleClick] = useState({left:0, right:0})
+  // const [allClicks, setAll] = useState([]);
+  // const [total, setTotal] = useState(0);
+  const [value, setValue] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const handleLeftClick=()=>{
-    setAll(allClicks.concat("L"));
-    handleClick({...click, left: click.left+1});
+  const handleClick=()=>{
+    const newVal = value +1;
+    setValue(newVal);
+    console.log(newVal);
+    setTotal(newVal + 2);
   }
 
-  const handleRightClick=()=>{
-    setAll(allClicks.concat("U"));
-    handleClick({...click, right:click.right+1});
-  }
+  // const handleLeftClick=()=>{
+  //   setAll(allClicks.concat("L"));
+  //   const updateLeft ={...click, left: click.left+1};
+  //   handleClick(updateLeft);
+  //   setTotal(updateLeft.left + click.right);
+
+
+  // }
+
+  // const handleRightClick=()=>{
+  //   setAll(allClicks.concat("R"));
+  //   handleClick({...click, right:click.right+1});
+  //   console.log("right click",click.right)
+
+    //const updateRight = {...click, right:click.right+1};
+    // handleClick(updateRight);
+    // setTotal(click.left + updateRight.right);
+  //}
 
   return (
     <>
-      <div>
+    <p>value {value}</p>
+    <p>total{total}</p>
+
+    <button onClick={handleClick}>btn</button>
+      {/* <div>
         <p>update arr {allClicks.join(" ")}</p>
         <p>The left btn is clicked {click.left} times</p>
         <button onClick={handleLeftClick}>Left BTN</button>
-      </div>
-      <div>
         <p>The right btn is clicked {click.right} times</p>
         <button onClick={handleRightClick}>Right BTN</button>
-      </div>
+        <p>The Total {total}</p>
+      </div> */}
     </>
   );
 }
