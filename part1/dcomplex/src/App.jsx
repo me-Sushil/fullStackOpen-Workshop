747bbb4b42b75ffc2abcd86190a2f6e3cee4497f
me-Sushil <girbahadurbk123@gmail.com>
2025-08-05 15:30:44 +0545

    directly increase not work

--- part1/dcomplex/src/App.jsx
+++ part1/dcomplex/src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 
 function App() {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  // const [left, setLeft] = useState(0);
+  // const [right, setRight] = useState(0);
+  const [click, handleClick] = useState({left:0, right:0})
+
+  const handleLeftClick=()=>{
+    
+    handleClick(click.left + 1);
+
+  }
+
+  const handleRightClick=()=>{
+    handleClick(click.right + 1);
+  }
 
   return (
     <>
       <div>
-        <p>The left btn is clicked {left} times</p>
-        <button onClick={()=>setLeft(left +1)}>Left BTN</button>
+        <p>The left btn is clicked {click.left} times</p>
+        <button onClick={handleLeftClick}>Left BTN</button>
       </div>
       <div>
-        <p>The left btn is clicked {right} times</p>
-        <button onClick={()=>setRight(right +1)}>Right BTN</button>
+        <p>The left btn is clicked {click.right} times</p>
+        <button onClick={handleRightClick}>Right BTN</button>
       </div>
     </>
   );
