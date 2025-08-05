2171013130611149bbd727a7d9dfc05742b04614
me-Sushil <girbahadurbk123@gmail.com>
2025-08-06 00:26:41 +0545

    change body background color, white background effect my eye

--- part1/dcomplex/src/main.jsx
+++ part1/dcomplex/src/main.jsx
@@ -2,3 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
+const body = document.body;
+body.style.backgroundColor = '#7A7777';
