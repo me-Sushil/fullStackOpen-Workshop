ac7261e11cae2cee7dbed434ad6759049dcee973
me-Sushil <girbahadurbk123@gmail.com>
2025-08-05 15:16:21 +0545

    create 2 useState and handle by two button

--- part1/dcomplex/src/App.jsx
+++ part1/dcomplex/src/App.jsx
@@ -1,14 +1,21 @@
-//import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
- // const [count, setCount] = useState(0)
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
   return (
     <>
-    <p>Hello bro </p>
+      <div>
+        <p>The left btn is clicked {left} times</p>
+        <button onClick={()=>setLeft(left +1)}>Left BTN</button>
+      </div>
+      <div>
+        <p>The left btn is clicked {right} times</p>
+        <button onClick={()=>setRight(right +1)}>Right BTN</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
