bb79652b3185533c9a9a0b7c3d51c1683c1abcf7
me-Sushil <girbahadurbk123@gmail.com>
2025-08-06 22:29:15 +0545

    create object array and pass to app

--- part2/notes-app/src/main.jsx
+++ part2/notes-app/src/main.jsx
@@ -1,4 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+const notes = [
+  {id:1, content:"hello1"},
+  {id:2, content:"hello2"},
+  {id:3, content:"hello3"}
+]
+
+createRoot(document.getElementById('root')).render(<App notes={notes}/>)
