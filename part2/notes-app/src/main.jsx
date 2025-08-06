7e0234ed3fc26d238ea2578d45b4caf14c35f219
me-Sushil <girbahadurbk123@gmail.com>
2025-08-06 22:57:17 +0545

    add importent key in object

--- part2/notes-app/src/main.jsx
+++ part2/notes-app/src/main.jsx
@@ -2,9 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 const notes = [
-  {id:1, content:"hello1"},
-  {id:2, content:"hello2"},
-  {id:3, content:"hello3"}
+  {id:1, content:"hello1", important: true},
+  {id:2, content:"hello2", important: false},
+  {id:3, content:"hello3", important: true}
 ]
 
 createRoot(document.getElementById('root')).render(<App notes={notes}/>)
