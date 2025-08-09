cab760d5e7d6aee03ff7c53041697d82d0543c3e
me-Sushil <girbahadurbk123@gmail.com>
2025-08-09 14:07:14 +0545

    add importent key in object and handle

--- part2/form/src/App.jsx
+++ part2/form/src/App.jsx
@@ -4,13 +4,14 @@ import Note from "./component/Note";
 const App=(props)=>{
 
   const [notes, setNotes]= useState(props.notes);
-  const [newnote, setNewNote] = useState("type somthing");
+  const [newnote, setNewNote] = useState("");
   function handleSubmit(event){
     event.preventDefault();
     console.log("form submit", event.target);
      const newObj = {
       id:notes.length+1,
-      note:newnote
+      note:newnote,
+      important: Math.random() > 0.5
     }
     setNotes(notes.concat(newObj));
     setNewNote("");
@@ -29,7 +30,7 @@ const App=(props)=>{
     )}
     </ul>
     <form onSubmit={handleSubmit}>
-      <input value={newnote} onChange={handleInput}/>
+      <input  placeholder="Type here" value={newnote} onChange={handleInput}/>
       <button>submit</button>
     </form>
     </>
