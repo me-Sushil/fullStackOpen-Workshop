b4e250016c46d22f1d3a2cc73c15fbd5b6a34c59
me-Sushil <girbahadurbk123@gmail.com>
2025-08-09 13:30:23 +0545

    add important key in object

--- part2/form/src/main.jsx
+++ part2/form/src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 const notes = [
-{ id:1, note:"HTML is easy"},
-{ id:2, note: "Browser can execute only JavaScript"},
-{id:3, note: "GET and POST  are the most important methods of HTTP protocol"}]
+{ id:1, note:"HTML is easy", important: true},
+{ id:2, note: "Browser can execute only JavaScript", important: false},
+{id:3, note: "GET and POST  are the most important methods of HTTP protocol", important:true}]
 
 createRoot(document.getElementById('root')).render(<App notes={notes}/>)
