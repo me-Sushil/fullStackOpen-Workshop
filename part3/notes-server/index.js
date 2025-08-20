b041cebe2fb8c95a465e49645abb650cb6b68700
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-21 00:50:46 +0545

    add:  notes array data

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,8 +1,26 @@
 const http = require('http')//it import the http package,  no need to install it's build in package
 
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
 const app = http.createServer((request, response) => {
-  response.writeHead(404, { 'Content-Type': 'text/notplain' })
-  response.end('Hello World')
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
 })
 
 const PORT = 3001
