9a40d8315ac8292c83bbe80ee0ba30d49114ac2c
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-21 12:19:47 +0545

    create: post

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,53 +1,62 @@
 // const http = require('http')//it import the http package,  no need to install it's build in package
 const express = require("express");
 const app = express();
+app.use(express.json());
+
 let notes = [
   {
     id: "1",
     content: "HTML is easy",
-    important: true
+    important: true,
   },
   {
     id: "2",
     content: "Browser can execute only JavaScript",
-    important: false
+    important: false,
   },
   {
     id: "3",
     content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+    important: true,
+  },
+];
 
 // const app = http.createServer((request, response) => {
 //   response.writeHead(200, { 'Content-Type': 'application/json' })
 //   response.end(JSON.stringify(notes))
 // })
 
-app.get("/", (request, response)=>{
-    response.send("<h2>Hello World!</h2>")
-})
+app.get("/", (request, response) => {
+  response.send("<h2>Hello World!</h2>");
+});
 
-app.get("/api/notes", (request, response)=>{
-    response.json(notes)
-})
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
 
-app.get("/api/notes/:noteid", (request, response)=>{
-    const nid = request.params.noteid;
-    const responseid = notes.find(note=> note.id === nid);
-    if(responseid){
+app.get("/api/notes/:noteid", (request, response) => {
+  const nid = request.params.noteid;
+  const responseid = notes.find((note) => note.id === nid);
+  if (responseid) {
     response.json(responseid);
-    }else{
-        response.status(404).end();
-    }
-})
+  } else {
+    response.status(404).end();
+  }
+});
 
-app.delete("/api/notes/:noteid", (request, response)=>{
-    const nid = request.params.noteid;
-    notes = notes.filter(note=> note.id !== nid);
-    response.status(204).end();
-} )
+app.delete("/api/notes/:noteid", (request, response) => {
+  const nid = request.params.noteid;
+  notes = notes.filter((note) => note.id !== nid);
+  response.status(204).end();
+});
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.post("/api/notes/", (request, response) => {
+   const data = request.body;
+notes.push(data);
+response.json(notes);
+
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
