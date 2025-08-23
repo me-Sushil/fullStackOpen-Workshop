445232e106da11941683afae2b2be85fdbb50bc3
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-23 22:21:17 +0545

    copy build dist into server

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-// app.use(express.static("dist"));
+app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
