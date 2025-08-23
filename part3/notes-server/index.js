77aaceb937a0342069ea8d79adda7170c96793b0
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-23 18:44:49 +0545

    import : cors

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,7 +1,9 @@
 // const http = require('http')//it import the http package,  no need to install it's build in package
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
