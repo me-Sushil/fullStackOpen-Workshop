6b8de6107b7ada74311eac96cc252edd88aec5c6
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-24 12:41:29 +0545

    change : listening port import

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -99,6 +99,5 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT ? process.env.PORT: 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT);
+console.log(`Server running on port ${process.env.PORT}`);
