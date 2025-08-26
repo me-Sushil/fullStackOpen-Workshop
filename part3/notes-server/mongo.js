67c9d227a01342fba6afbd7567e530e1a64e3e79
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-26 23:41:33 +0545

    edit:   code according eslint error handling

--- part3/notes-server/mongo.js
+++ part3/notes-server/mongo.js
@@ -1,31 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-
 const url = process.env.MONGODB_URI;
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-  content: 'HTML is easy',
+  content: "HTML is easy",
   important: true,
 })
-
 note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+  console.log('note saved!');
+  mongoose.connection.close();
+});
