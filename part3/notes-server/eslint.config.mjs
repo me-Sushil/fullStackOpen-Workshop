2b197e4caf5fb4debc376e103eaca71899623a79
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-29 23:52:05 +0545

    comment style checker in eslint

--- part3/notes-server/eslint.config.mjs
+++ part3/notes-server/eslint.config.mjs
@@ -1,6 +1,6 @@
 import globals from "globals";
 import js from "@eslint/js";
-import stylisticJs from "@stylistic/eslint-plugin-js";
+// import stylisticJs from "@stylistic/eslint-plugin-js";
 
 export default [
   js.configs.recommended,
@@ -11,9 +11,9 @@ export default [
       globals: { ...globals.node },
       ecmaVersion: "latest",
     },
-    plugins: {
-      "@stylistic/js": stylisticJs,
-    },
+    // plugins: {
+    //   "@stylistic/js": stylisticJs,
+    // },
     rules: {
       "@stylistic/js/indent": ["error", 2],
       "@stylistic/js/linebreak-style": ["error", "unix"],
