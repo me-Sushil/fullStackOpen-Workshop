a716f6336eaf5965b4d04775d7a88afb2ecc8e89
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-31 17:35:09 +0545

    change :  file and folder for code

---
+++ part3/notes-server/model/note.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const config = require("../utils/config");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then((result) => {
+    console.log("Connected to the mongoDB");
+  })
+  .catch((error) => {
+    console.log(error.message, "Error while connectiong to the Mongodb");
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
