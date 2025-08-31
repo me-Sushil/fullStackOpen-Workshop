bed117443c3f15cab8e2d421bc93dafb462786cc
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-31 17:48:39 +0545

    seperate all notes endpoint to notes file

--- part3/notes-server/controllers/notes.js
+++ part3/notes-server/controllers/notes.js
@@ -1,31 +1,62 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-const config = require("../utils/config");
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(config.MONGODB_URI)
-  .then((result) => {
-    console.log("Connected to the mongoDB");
-  })
-  .catch((error) => {
-    console.log(error.message, "Error while connectiong to the Mongodb");
-  });
+const Note = require("../model/note");
+const noteRouter = require("express").Router();
 
-const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    minlength: 5,
-    required: true,
-  },
-  important: Boolean,
+noteRouter.get("/", (request, response, next) => {
+  Note.find({})//find without any paramiter to gett all 
+    .then((result) => response.json(result))// use json to send json format
+    .catch((error) => {
+      next(error);
+    });
 });
 
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+noteRouter.get("/:noteid", (request, response, next) => {
+  const nid = request.params.noteid;
+  Note.findById(nid)
+    .then((result) => response.json(result))
+    .catch((error) => next(error));
 });
 
-module.exports = mongoose.model("Note", noteSchema);
+noteRouter.delete("/:noteid", (request, response, next) => {
+  const nid = request.params.noteid;
+  Note.findByIdAndDelete(nid)
+    .then((result) => response.status(204).end())
+    .catch((error) => {
+      next(error);
+    });
+});
+
+
+noteRouter.post("/", (request, response, next) => {
+  const { content, important } = request.body;
+
+  if (!content) {
+    return response.status(400).json({
+      error: "content is missing",
+    });
+  }
+
+  Note.findOne({ content: content })
+    .then((isExist) => {
+      if (isExist) {
+        return response.status(400).json({ error: "content must be unique" });
+      }
+      
+      // Move note creation inside the then block
+      const note = new Note({
+        content: content,
+        important: important || false,
+      });
+
+      return note.save();
+    })
+    .then((result) => {
+      if (result) {
+        response.json(result);
+        console.log("note saved!");
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+module.exports = noteRouter;
