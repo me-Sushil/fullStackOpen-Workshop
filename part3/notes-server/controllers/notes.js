5effd2bc165fd48adc05cca816c0709eb251beab
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-02 23:33:46 +0545

    Change : change all  promice chaining to async await

--- part3/notes-server/controllers/notes.js
+++ part3/notes-server/controllers/notes.js
@@ -22,45 +22,44 @@ noteRouter.get("/:noteid", async (request, response, next) => {
 
 noteRouter.delete("/:noteid", async (request, response, next) => {
   const nid = request.params.noteid;
-  try{
-    Note.findByIdAndDelete(nid)
-    response.status(204).end()
-  }catch(error){
+  try {
+    Note.findByIdAndDelete(nid);
+    response.status(204).end();
+  } catch (error) {
     next(error);
   }
 });
 
-noteRouter.post("/", (request, response, next) => {
-  const { content, important } = request.body;
+noteRouter.post("/", async (request, response, next) => {
+  try {
+    const { content, important } = request.body;
 
-  if (!content) {
-    return response.status(400).json({
-      error: "content is missing",
-    });
-  }
-
-  Note.findOne({ content: content })
-    .then((isExist) => {
-      if (isExist) {
-        return response.status(400).json({ error: "content must be unique" });
-      }
-
-      // Move note creation inside the then block
-      const note = new Note({
-        content: content,
-        important: important || false,
+    if (!content) {
+      return response.status(400).json({
+        error: "content is missing",
       });
+    }
 
-      return note.save();
-    })
-    .then((result) => {
-      if (result) {
-        response.status(201).json(result);
-        console.log("note saved!");
-      }
-    })
-    .catch((error) => {
-      next(error);
+    const isExist = await Note.findOne({ content: content });
+
+    if (isExist) {
+      return response.status(400).json({ error: "content must be unique" });
+    }
+
+    // Move note creation inside the then block
+    const note = new Note({
+      content: content,
+      important: important || false,
     });
+
+    const result = await note.save();
+
+    if (result) {
+      response.status(201).json(result);
+      console.log("note saved!");
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 module.exports = noteRouter;
