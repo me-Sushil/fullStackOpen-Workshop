f0291071a9fd6d3ca5ed114e7441a0a581c05af7
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-03 23:58:14 +0545

    Add:  test for user

--- part3/notes-server/tests/note_api.test.js
+++ part3/notes-server/tests/note_api.test.js
@@ -5,8 +5,8 @@ const supertest = require("supertest");
 const Note = require("../model/note");
 const app = require("../app");
 const helper = require("./test_helper");
-const bcrypt = require('bcrypt')
-const User = require('../models/user')
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
 const api = supertest(app);
 
@@ -102,6 +102,39 @@ describe("when there is initially some notes saved", () => {
     });
   });
 });
+describe("when there is initially one user at db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("sekret", 10);
+    const user = new User({ username: "root", passwordHash });
+
+    await user.save();
+  });
+
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "mluukkai",
+      name: "Matti Luukkainen",
+      password: "salainen",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    assert(usernames.includes(newUser.username));
+  });
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
