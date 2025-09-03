cc6f5dcc29a77a51577935c055adc18968ad4acc
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-04 00:05:39 +0545

    Correct : import,  model from models

--- part3/notes-server/tests/note_api.test.js
+++ part3/notes-server/tests/note_api.test.js
@@ -6,7 +6,7 @@ const Note = require("../model/note");
 const app = require("../app");
 const helper = require("./test_helper");
 const bcrypt = require("bcrypt");
-const User = require("../models/user");
+const User = require("../model/user");
 
 const api = supertest(app);
 
