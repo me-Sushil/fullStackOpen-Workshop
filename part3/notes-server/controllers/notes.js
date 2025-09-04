d36fdcaa80c1f5b0a3fe4e95b8c066f3f1d0812b
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-04 23:23:21 +0545

    get token fron request decoded, get user by decoded id and save note and user

--- part3/notes-server/controllers/notes.js
+++ part3/notes-server/controllers/notes.js
@@ -1,6 +1,8 @@
 const Note = require("../model/note");
 const noteRouter = require("express").Router();
 const User = require("../model/user");
+const config = require("../utils/config");
+const jwt = require("jsonwebtoken");
 
 noteRouter.get("/", async (request, response, next) => {
   try {
@@ -41,22 +43,27 @@ noteRouter.delete("/:noteid", async (request, response, next) => {
 
 noteRouter.post("/", async (request, response, next) => {
   try {
-    const {userId, content,important } = request.body;
+    const authorization = request.get("authorization");
+    console.log(authorization, "this is token from request");
 
-    const user = await User.findById(userId);
-    console.log(userId, content, important, " this is body content");
-   console.log(user);
-    if (!user) {
+    const authoArr = authorization.split(" ");
+
+    const decodedToken = jwt.verify(authoArr[1], config.SEKRET);
+    console.log(decodedToken, "this is decoded token");
+    if(!decodedToken){
+      return response.status(401).json({error:"Unauthorized"});
+    }
+
+      const { content,important } = request.body;
+
+      const user = await User.findById(decodedToken.id);
+      console.log(user, " get by decoded token");
+
+      if (!user) {
       return response
         .status(400)
         .json({ error: "userId missing or not valid" });
     }
-
-    // if (!body.content) {
-    //   return response.status(400).json({
-    //     error: "content is missing",
-    //   });
-    // }
 
     const isExist = await Note.findOne({ content });
 
@@ -71,9 +78,9 @@ noteRouter.post("/", async (request, response, next) => {
       user: user.id,
     });
 
-    const result = await note.save();
-    user.notes = user.notes.concat(result.id);
-    await user.save();
+     const result = await note.save();
+     user.notes = user.notes.concat(result.id);
+     await user.save();
 
     if (result) {
       response.status(201).json(result);
@@ -83,4 +90,5 @@ noteRouter.post("/", async (request, response, next) => {
     next(error);
   }
 });
+
 module.exports = noteRouter;
