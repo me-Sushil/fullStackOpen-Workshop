4e54d723992109aae4f7a35adb7d48e270475d83
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-04 23:22:35 +0545

    handle error

--- part3/notes-server/utils/middleware.js
+++ part3/notes-server/utils/middleware.js
@@ -1,16 +1,16 @@
-const logger = require('./logger')
+const logger = require("./logger");
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 const errorhandler = (error, request, response, next) => {
   console.error(error.message);
@@ -18,22 +18,27 @@ const errorhandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     // Invalid ObjectId format (e.g., malformed MongoDB _id)
     return response.status(400).send({ error: "malformatted id" });
-
   } else if (error.name === "ValidationError") {
     // Mongoose schema validation failed (invalid or missing data)
     return response.status(400).json({ error: error.message });
-
-  } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
-    return response.status(400).json({ error: 'expected `username` to be unique' })
+  } else if (
+    error.name === "MongoServerError" &&
+    error.message.includes("E11000 duplicate key error")
+  ) {
+    return response
+      .status(400)
+      .json({ error: "expected `username` to be unique" });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.name === "ReferenceError") {
+    return response.status(400).json({ error: error.message });
   }
-
 
   next(error);
 };
 
-
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorhandler
-}
+  errorhandler,
+};
