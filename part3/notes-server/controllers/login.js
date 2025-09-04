284e31f9581188eb5cc6a8b5c4115a9035ccddfd
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-04 20:17:59 +0545

    create :   JWT

--- part3/notes-server/controllers/login.js
+++ part3/notes-server/controllers/login.js
@@ -1,11 +1,32 @@
 const bcrypt = require("bcrypt");
 const User = require("../model/user");
 const loginRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
 
-loginRouter.post("/", (request, response)=>{
-    const {username, password} = request.body;
+loginRouter.post("/", async (request, response) => {
+  const { username, password } = request.body;
 
-    response.status(200).send("test success");
+  const user = await User.findOne({ username });
+
+  console.log(user, " user ");
+
+  const isPasswordValid =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+
+  console.log(isPasswordValid, "is password right");
+
+  if (!(isPasswordValid && user)) {
+    return response.status(401).json({ error: "invalid username or password" });
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
+
+  const token = jwt.sign(userForToken, "my sekret");
+  console.log("token", token);
+  response.status(200).json({token:token, username:user.username, name:user.name});
 });
 
 module.exports = loginRouter;
