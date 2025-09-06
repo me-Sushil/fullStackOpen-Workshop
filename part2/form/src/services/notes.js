21cdb205ff37cc567a11d16064ba46acf11af269
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-06 23:51:44 +0545

    add : token

--- part2/form/src/services/notes.js
+++ part2/form/src/services/notes.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const baseUrl = '/api/notes'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) =>
