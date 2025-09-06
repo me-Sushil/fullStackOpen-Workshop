29bc937c2f7e9caeb722f36920e43b56d884c0a6
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-06 23:22:53 +0545

    Add : Login form

--- part2/form/src/App.jsx
+++ part2/form/src/App.jsx
@@ -10,7 +10,8 @@ const App = () => {
   const [newnote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState("All good now...");
-
+const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
       setNotes(initialNotes);
@@ -108,11 +109,39 @@ const App = () => {
   const handleShowFilter = () => {
     setShowAll(!showAll);
   };
+   const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
 
   return (
     <>
       <h1>My Notes</h1>
       <Notification message={errorMessage} />
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
       <button onClick={handleShowFilter}>
         Show {showAll ? "important" : "all"}
       </button>
