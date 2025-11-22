688486706c103630d1c963c8c8e2a49ed3450c56
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-22 17:37:28 +0545

    Add : state and function inside component

--- part2/form/src/component/LoginForm.jsx
+++ part2/form/src/component/LoginForm.jsx
@@ -1,33 +1,31 @@
-  const LoginForm = ({handleLogin,
-   setUsername,
-   setPassword,
-   username,
-   password}) => {
-    return (
-        <>
-    <form onSubmit={handleLogin}>
-      <div>
-        <label>
-          username
-          <input
-            type="text"
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          password
-          <input
-            type="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </label>
-      </div>
-      <button type="submit">login</button>
-    </form>
+const LoginForm = (props) => {
+  return (
+    <>
+      <h2>Login</h2>
+      <form onSubmit={props.handleSubmit}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={props.username}
+              onChange={props.handleUsernameChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={props.password}
+              onChange={props.handlePasswordChange}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
     </>
-  )}
+  );
+};
 export default LoginForm;
