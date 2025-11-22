9c2f6fd6499e5b8db1987d9e0f110a51f2fdcde2
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-22 17:52:40 +0545

    Use :  useRef  to lift state or function from child to parent

--- part2/form/src/App.jsx
+++ part2/form/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Note from "./component/Note";
 import noteService from "./services/notes";
 import loginService from "./services/login";
@@ -16,6 +16,7 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const noteFormRef = useRef();
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -34,6 +35,8 @@ const App = () => {
   }, []);
 
   const addNote = (noteObject) => {
+    noteFormRef.current.toggleVisibility();
+
     noteService.create(noteObject).then((returnedNote) => {
       setNotes(notes.concat(returnedNote));
     });
@@ -117,7 +120,7 @@ const App = () => {
 
   const noteForm = () => {
     return (
-      <Togglable buttonLabel="new note">
+      <Togglable buttonLabel="new note" ref={noteFormRef}>
         <NoteForm createNote={addNote} />
       </Togglable>
     );
--- part2/form/src/component/Togglable.jsx
+++ part2/form/src/component/Togglable.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useState, useImperativeHandle } from "react";
 
 const Togglable = (props) => {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
 
   const toggleVisibility = () => {
-    setVisible(!visible)
-  }
+    setVisible(!visible);
+  };
+
+  useImperativeHandle(props.ref, () => {
+    return { toggleVisibility };
+  });
 
   return (
     <div>
@@ -16,12 +20,11 @@ const Togglable = (props) => {
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
       <div style={showWhenVisible}>
-
         {props.children}
         <button onClick={toggleVisibility}>cancel</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Togglable
+export default Togglable;
