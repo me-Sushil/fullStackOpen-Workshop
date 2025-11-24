cfcd407e332b21b48156aaec9657c40bfa241266
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-24 16:35:11 +0545

    Clicking Button test :  Pass

--- part2/form/src/component/Note.jsx
+++ part2/form/src/component/Note.jsx
@@ -4,7 +4,7 @@ const Note = ({ note, toggleImportance }) => {
   return (
     <>
       <li className='note'>
-       Your awesome note: {note.content}
+        {note.content}
         <button onClick={toggleImportance}>{label}</button>
       </li>
     </>
--- part2/form/src/component/Note.test.jsx
+++ part2/form/src/component/Note.test.jsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import Note from "./Note";
 
 test("renders content", () => {
@@ -39,4 +41,21 @@ test("renders content with container", () => {
   expect(div).toHaveTextContent(
     "Component testing is done with react-testing-library"
   );
+});
+
+test("clicking the button calls event handler once", async () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  const mockHandler = vi.fn();
+
+  render(<Note note={note} toggleImportance={mockHandler} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("false");
+  await user.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
