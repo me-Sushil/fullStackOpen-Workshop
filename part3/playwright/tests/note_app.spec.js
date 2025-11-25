1f7341dd9de75f146bd169b391ca82473d351759
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-25 17:03:46 +0545

    Add : base url into config

--- part3/playwright/tests/note_app.spec.js
+++ part3/playwright/tests/note_app.spec.js
@@ -9,8 +9,8 @@ const { createNote, loginWith } = require("./helper");
 
 describe("Note app", () => {
   beforeEach(async ({ page, request }) => {
-    await request.post("http://localhost:3001/api/testing/reset");
-    await request.post("http://localhost:3001/api/users", {
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
       data: {
         name: "Sushil BK",
         username: "Sushil1",
@@ -18,7 +18,7 @@ describe("Note app", () => {
       },
     });
 
-    await page.goto("http://localhost:5173");
+    await page.goto("/");
   });
   test("front page can be opened", async ({ page }) => {
     const locator = page.getByText("Notes");
