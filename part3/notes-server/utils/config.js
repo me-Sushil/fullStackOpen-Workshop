f67a4aad7e004b842f38b8a1c958deea8cb81bc7
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-25 14:15:42 +0545

    make changes to create user in  testDB

--- part3/notes-server/utils/config.js
+++ part3/notes-server/utils/config.js
@@ -1,6 +1,8 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT ? process.env.PORT:3001;
-const MONGODB_URI = process.env.NODE_ENV=== "test" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
+const PORT = process.env.PORT ? process.env.PORT : 3001;
+// const MONGODB_URI = process.env.NODE_ENV=== "test" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
+const MONGODB_URI = process.env.TEST_MONGODB_URI;
+
 const SEKRET = process.env.SEKRET;
-module.exports = { MONGODB_URI, PORT, SEKRET }
+module.exports = { MONGODB_URI, PORT, SEKRET };
