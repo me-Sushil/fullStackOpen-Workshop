60b4c07bc58cc69816deaf98837f433264e9891f
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-27 17:39:01 +0545

    seprate the notes

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,25 +1,10 @@
 import NoteForm from "./components/NoteForm";
-import { toggleImportanceOf } from "./reducers/noteReducer";
-import { useSelector, useDispatch } from 'react-redux' 
-
+import Notes from "./components/Notes";
 function App() {
- const dispatch = useDispatch()
-  const notes = useSelector(state => state)
-
   return (
     <>
-    <NoteForm/>
-      <ul>
-        {notes.map((note) => (
-          <li
-            key={note.id}
-            onClick={() => dispatch(toggleImportanceOf(note.id))}
-          >
-            {note.content}{" "}
-            <strong>{note.important ? "important" : "not important"}</strong>
-          </li>
-        ))}
-      </ul>
+      <NoteForm />
+      <Notes/>
     </>
   );
 }
