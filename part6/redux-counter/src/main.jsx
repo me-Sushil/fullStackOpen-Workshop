bd3614796b7bfd3d682d9bd15178d9d9df45d8d8
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-27 13:46:02 +0545

    Impliment the basic , or fundamental of redux to increment

--- part6/redux-counter/src/main.jsx
+++ part6/redux-counter/src/main.jsx
@@ -1,21 +1,43 @@
 import { createRoot } from "react-dom/client";
 import { useState } from "react";
 import "./index.css";
+import { createStore } from "redux";
+
+const counterReducer = (state=0, action) => {
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  } else if (action.type === "DECREMENT") {
+    return state - 1;
+  } else if (action.type === "ZERO") {
+    return 0;
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const incriment =()=>{
-    setCount(count+1);
-  }
+  const incriment = () => {
+    // setCount(count + 1);
+    store.dispatch({ type: "INCREMENT" });
+  };
   return (
     <>
-      <div>the count is {count}</div>
+      {/* <div>the count is {count}</div> */}
+      <div>the count is {store.getState()}</div>
       <div className="card">
         <button onClick={incriment}>Increment</button>
       </div>
     </>
   );
 }
-
-createRoot(document.getElementById("root")).render(<App />);
+// createRoot(document.getElementById("root")).render(<App />);
+const myRoot = createRoot(document.getElementById("root"));
+function myRender() {
+  myRoot.render(<App />);
+}
+myRender();
+store.subscribe(myRender);
