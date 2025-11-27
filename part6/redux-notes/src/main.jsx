8aeb57202ccab3e8c83917f5b5c7db4cdf7c6cca
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-27 14:21:13 +0545

    set NoteApp for redux

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,10 +1,56 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createStore } from "redux";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case "NEW_NOTE":
+      state.push(action.payload);
+      return state;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
+
+function App() {
+  return (
+    <>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? "important" : "not important"}</strong>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+// createRoot(document.getElementById("root")).render(<App />);
+const myRoot = createRoot(document.getElementById("root"));
+function renderApp() {
+  myRoot.render(<App />);
+}
+renderApp();
+store.subscribe(renderApp); //every time store value change that time
+// myRender function call and this will render the app
