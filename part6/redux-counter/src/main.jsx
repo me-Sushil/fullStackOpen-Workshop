01a93e05dd49344676a50942bdd82ef73494de7f
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-27 13:27:28 +0545

    make counter with state

--- part6/redux-counter/src/main.jsx
+++ part6/redux-counter/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  const [count, setCount] = useState(0);
+
+  const incriment =()=>{
+    setCount(count+1);
+  }
+  return (
+    <>
+      <div>the count is {count}</div>
+      <div className="card">
+        <button onClick={incriment}>Increment</button>
+      </div>
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
