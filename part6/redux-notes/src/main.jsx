33d5fe83e6c909589a5c987db22d520190cd2bf9
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-30 22:19:39 +0545

    import react quiry app

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./ReactQueryApp";
 import { Provider } from "react-redux";
 import "./index.css";
 import store from './store'
