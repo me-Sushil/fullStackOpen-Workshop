9cfc4b9015eb425c930e2396861e6c8c9feb4095
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-30 22:46:48 +0545

    Get db data

--- part6/redux-notes/src/ReactQueryApp.jsx
+++ part6/redux-notes/src/ReactQueryApp.jsx
@@ -1,16 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const App = () => {
-  const addNote = async (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
+  //   const result = useQuery({
+  //     queryKey: ["notes"],
+  //     queryFn: async () => {
+  //       await axios
+  //         .get("http://localhost:3001/notes")
+  //         .then((response) => response.data);
+  //     },
+  //   });
+  //   if (result.isLoading) {
+  //     return <div>loading data...</div>;
+  //   }
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: async () => {
+      const response = await axios.get("http://localhost:3001/notes");
+      return response.data
+    },
+  });
+
+     console.log(result, " this is query data");
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
   }
+
+  const notes = result.data;
+
+  const addNote = async (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    console.log(content);
+  };
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
-  }
+    console.log("toggle importance of", note.id);
+  };
 
-  const notes = []
+  //   const notes = [];
 
   return (
     <div>
@@ -22,11 +52,11 @@ const App = () => {
       {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
           {note.content}
-          <strong> {note.important ? 'important' : ''}</strong>
+          <strong> {note.important ? "important" : ""}</strong>
         </li>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
