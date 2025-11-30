f051b11448a416757ac227b0f49743dfad6ea0b0
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-30 11:43:11 +0545

    get all notes and dispatch

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,12 +1,20 @@
+import { useDispatch } from "react-redux";
 import NoteForm from "./components/NoteForm";
 import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
+import { getAll } from "./services/notes";
+import { addAllNotes } from "./reducers/noteReducer";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getAll().then((data) => dispatch(addAllNotes(data)));
+  }, []);
   return (
     <>
       <NoteForm />
-      <VisibilityFilter/>
+      <VisibilityFilter />
       <Notes />
     </>
   );
