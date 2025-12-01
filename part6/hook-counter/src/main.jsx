022d5e76d5fff6c6873202bc729edf45809427ac
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-01 18:39:56 +0545

    Applyed : useContext and useReducer

--- part6/hook-counter/src/main.jsx
+++ part6/hook-counter/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
+import { CounterContextProvider } from "./CounterContext";
 
-import App from './App'
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <CounterContextProvider>
     <App />
-  </StrictMode>,
-)
+  </CounterContextProvider>
+);
