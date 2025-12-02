b21e2f02581afeb04cdd94bb32de37bd3ca08852
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-02 10:30:59 +0545

    comment app and impliment to main.jsx

--- part7/react-router/src/main.jsx
+++ part7/react-router/src/main.jsx
@@ -1,10 +1,76 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+const Home = () => (
+  <div>
+    {" "}
+    <h2>TKTL notes app</h2>{" "}
+  </div>
+);
+
+const Notes = () => (
+  <div>
+    {" "}
+    <h2>Notes</h2>{" "}
+  </div>
+);
+
+const Users = () => (
+  <div>
+    {" "}
+    <h2>Users</h2>{" "}
+  </div>
+);
+
+const App = () => {
+  const [page, setPage] = useState("home");
+
+  const toPage = (page) => (event) => {
+    event.preventDefault();
+    setPage(page);
+  };
+
+  const content = () => {
+    if (page === "home") {
+      return <Home />;
+    } else if (page === "notes") {
+      return <Notes />;
+    } else if (page === "users") {
+      return <Users />;
+    }
+  };
+
+  const padding = {
+    padding: 5,
+  };
+
+  return (
+    <div>
+      <div>
+        <a href="" onClick={toPage("home")} style={padding}>
+          home
+        </a>
+        <a href="" onClick={toPage("notes")} style={padding}>
+          notes
+        </a>
+        <a href="" onClick={toPage("users")} style={padding}>
+          users
+        </a>
+      </div>
+
+      {content()}
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
