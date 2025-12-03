0951e1ea170775bb15fddaec7f477f80471576b0
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-03 09:55:15 +0545

    create second count  by same useCounter

--- part7/custom-hooks/src/App.jsx
+++ part7/custom-hooks/src/App.jsx
@@ -3,12 +3,23 @@ import useCounter from "./hooks/useCounter";
 
 function App() {
   const count = useCounter();
+  const count2 = useCounter();
   return (
     <>
-      <h1>{count.counter}</h1>
-      <button onClick={count.handlePlus}>plus</button>
-      <button onClick={count.handleMinus}>minus</button>
-      <button onClick={count.handleZero}>zero</button>
+      <div style={{ display: "flex" }}>
+        <div>
+          <h1>{count.counter}</h1>
+          <button onClick={count.handlePlus}>plus</button>
+          <button onClick={count.handleMinus}>minus</button>
+          <button onClick={count.handleZero}>zero</button>
+        </div>
+        <div>
+          <h1>{count2.counter}</h1>
+          <button onClick={count2.handlePlus}>plus</button>
+          <button onClick={count2.handleMinus}>minus</button>
+          <button onClick={count2.handleZero}>zero</button>
+        </div>
+      </div>
     </>
   );
 }
