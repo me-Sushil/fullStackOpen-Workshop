435f4c5023fc571243de2e212c38c58c265853bd
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-03 10:02:58 +0545

    import and list counter component in app

--- part7/custom-hooks/src/App.jsx
+++ part7/custom-hooks/src/App.jsx
@@ -1,25 +1,10 @@
 import "./App.css";
-import useCounter from "./hooks/useCounter";
-
+import Counter from "./Counter";
+import FormField from "./FormField";
 function App() {
-  const count = useCounter();
-  const count2 = useCounter();
   return (
     <>
-      <div style={{ display: "flex" }}>
-        <div>
-          <h1>{count.counter}</h1>
-          <button onClick={count.handlePlus}>plus</button>
-          <button onClick={count.handleMinus}>minus</button>
-          <button onClick={count.handleZero}>zero</button>
-        </div>
-        <div>
-          <h1>{count2.counter}</h1>
-          <button onClick={count2.handlePlus}>plus</button>
-          <button onClick={count2.handleMinus}>minus</button>
-          <button onClick={count2.handleZero}>zero</button>
-        </div>
-      </div>
+      <FormField />
     </>
   );
 }
