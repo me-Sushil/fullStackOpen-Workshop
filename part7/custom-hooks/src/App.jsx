684622bc6372d844e02adabd3cd810b8dc580981
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-03 09:51:47 +0545

    import hooks and impliment

--- part7/custom-hooks/src/App.jsx
+++ part7/custom-hooks/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import useCounter from "./hooks/useCounter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const count = useCounter();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{count.counter}</h1>
+      <button onClick={count.handlePlus}>plus</button>
+      <button onClick={count.handleMinus}>minus</button>
+      <button onClick={count.handleZero}>zero</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
