cb80d937c4623c95bbef5556c510d93d392ba472
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-04 18:25:11 +0545

    import express and get request to app

--- part13/notes-server/index.js
+++ part13/notes-server/index.js
@@ -1,16 +1,25 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { Sequelize, QueryTypes } = require("sequelize");
+const express = require("express");
+const app = express();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
+app.get("/api/notes", async (req, res) => {
+  const notes = await sequelize.query("SELECT * FROM notes", {
+    type: QueryTypes.SELECT,
+  });
+  res.json(notes);
+});
 
-main();
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
