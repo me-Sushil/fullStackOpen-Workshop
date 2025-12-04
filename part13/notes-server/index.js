aac16af467101da2a2101539583c484516451bc4
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-04 18:37:39 +0545

    get all notes using model

--- part13/notes-server/index.js
+++ part13/notes-server/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, DataTypes , Model } = require("sequelize");
 const express = require("express");
 const app = express();
 
@@ -12,10 +12,36 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
+class Note extends Model {}
+
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
+  }
+);
+
 app.get("/api/notes", async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", {
-    type: QueryTypes.SELECT,
-  });
+  const notes = await Note.findAll();
   res.json(notes);
 });
 
