4a90bdb12eca91fccb8440f01c80786feb7bb84b
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-06 13:20:01 +0545

    establish connection between notes and user

--- part13/notes-server/models/index.js
+++ part13/notes-server/models/index.js
@@ -1,8 +1,12 @@
 const Note = require("./note");
 const User = require('./user')
 
-Note.sync()
-User.sync()
+User.hasMany(Note)
+Note.belongsTo(User)
+
+Note.sync({ alter: true })
+User.sync({ alter: true })
+
 
 module.exports = {
   Note, User
