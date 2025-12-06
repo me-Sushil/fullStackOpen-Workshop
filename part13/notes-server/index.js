3f5ae235b021e35a72fdb769c75039ef20c13240
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-06 12:36:18 +0545

    Application structuring

--- part13/notes-server/index.js
+++ part13/notes-server/index.js
@@ -1,89 +1,20 @@
-require("dotenv").config();
-const { Sequelize, DataTypes, Model } = require("sequelize");
 const express = require("express");
 const app = express();
-app.use(express.json()); // request from user through url come in string
-//  that is why this code need to us for convert into object
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
 
-class Note extends Model {}
+const notesRouter = require("./controllers/notes");
 
-Note.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    important: {
-      type: DataTypes.BOOLEAN,
-    },
-    date: {
-      type: DataTypes.DATE,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "note",
-  }
-);
+app.use(express.json());
 
-Note.sync();
+app.use("/api/notes", notesRouter);
 
-app.get("/api/notes", async (req, res) => {
-  const notes = await Note.findAll();
-    console.log(JSON.stringify(notes, null, 2), "this is note");
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-  res.json(notes);
-});
-
-app.post("/api/notes", async (req, res) => {
-  console.log(req.body);
-  // whenn create is use we directly create and save, we cant modify in middle
-  const note = await Note.create(req.body);
-  res.json(note);
-
-  // when we use build, we can modify in middle, we need to save this after modify
-  //   const note = Note.build(req.body)
-  // note.important = true
-  // await note.save()
-});
-
-app.get("/api/notes/:id", async (req, res) => {
-  const note = await Note.findByPk(req.params.id);
-  if (note) {
-    console.log(note.toJSON(), "this is note");
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.put("/api/notes/:id", async (req, res) => {
-  const note = await Note.findByPk(req.params.id);
-  if (note) {
-    note.important = req.body.important;
-    await note.save();
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
-});
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
