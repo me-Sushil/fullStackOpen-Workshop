d7f8fa504a641e42fff887cc8ac3124c86cea25f
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-07 14:33:26 +0545

    add : admin  update user disable

--- part13/notes-server/controllers/users.js
+++ part13/notes-server/controllers/users.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-
+const { tokenExtractor } = require("../util/middleware");
 const { User } = require("../models");
 const { Note } = require("../models");
+
 router.get("/", async (req, res) => {
   // const users = await User.findAll()
   const users = await User.findAll({
@@ -30,5 +31,27 @@ router.get("/:id", async (req, res) => {
     res.status(404).end();
   }
 });
+const isAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  if (!user.admin) {
+    return res.status(401).json({ error: "operation not permitted" });
+  }
+  next();
+};
 
+router.put("/:username", tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+  });
+
+  if (user) {
+    user.disabled = req.body.disabled;
+    await user.save();
+    res.json(user);
+  } else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
