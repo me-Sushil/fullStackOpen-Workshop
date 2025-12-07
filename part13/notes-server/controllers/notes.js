ed6d07105383cc8c37afbfe87c88c10b19a730ef
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-07 14:32:20 +0545

    shift tokenExtractor and import

--- part13/notes-server/controllers/notes.js
+++ part13/notes-server/controllers/notes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
-const jwt = require("jsonwebtoken");
-const { SECRET } = require("../util/config");
+const { tokenExtractor } = require("../util/middleware");
 const { Note } = require("../models");
 const { User } = require("../models");
 const { Op } = require("sequelize");
@@ -27,21 +26,6 @@ router.get("/", async (req, res) => {
   res.json(notes);
 });
 
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      console.log(authorization.substring(7));
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch (error) {
-      console.log(error);
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
 router.post("/", tokenExtractor, async (req, res) => {
   try {
     const user = await User.findByPk(req.decodedToken.id);
